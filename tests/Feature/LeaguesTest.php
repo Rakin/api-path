be2eebc4a93586dba37d1b94974e119604cc9349
Rakin <rafael.kin.78@gmail.com>
2024-09-19 07:22:58 +0000

    Add testes

--- tests/Feature/LeaguesTest.php
+++ tests/Feature/LeaguesTest.php
@@ -1,10 +1,44 @@
 <?php
 
-test('list leagues actives', function () {
-    $fakeClient = getResponseClient();
+use Rakin\Poeninja\Client;
+use Rakin\Poeninja\CurrencyOverview;
+use Rakin\Poeninja\Enum\CurrencyType;
+use Rakin\Poeninja\Enum\ItemType;
+use Rakin\Poeninja\IndexState;
+use Rakin\Poeninja\ItemOverview;
 
-    $leaguesClass = new Leagues($fakeClient);;
+test('list IndexState', function () {
 
-    $leagues = $leaguesClass->get();
-    expect(count($leagues))->toBeGreaterThanOrEqualTo(1);
+    $indexState = new IndexState();
+
+    //$response = $indexState->get();
+
+    //expect($response->getStatusCode())->toBeBetween(200, 299);
 });
+
+test('list currencyoverview', function () {
+
+
+    foreach (CurrencyType::cases() as $type) {
+        $currencyOverview = new CurrencyOverview(
+            "Settlers",
+            $type
+        );
+        $response = $currencyOverview->get();
+
+        expect($response->getStatusCode())->toBeBetween(200, 299);
+    }
+});
+
+test('list itemoverview', function () {
+
+
+    foreach (ItemType::cases() as $type) {
+        $itemOverview = new ItemOverview(
+            getleague(),
+            $type
+        );
+        $response = $itemOverview->get();
+        expect($response->getStatusCode())->toBeBetween(200, 299);
+    }
+}); 
